cbaa0d925e754dc83f5fbed160ffb5857eb5cfa7
Nayoung1124 <53847442+Nayoung1124@users.noreply.github.com>
2020-07-27 14:04:42 +0900

    Create Day_06_YNY.py

---
+++ 오후반/30-days-of-code/Day_09/Day_06_YNY.py
@@ -0,0 +1,15 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+n=int(input())
+q_odd=[]
+q_even=[]
+for i in range (n):
+    q=str(input())
+    for j in range(len(q)):
+        if j%2==0:
+            q_odd.append(q[j])
+        if j%2==1:
+            q_even.append(q[j])
+    print("".join(q_odd) ,"".join(q_even))
+    q_odd,q_even=[],[]
+
